dd9460559fbd975bfc908d95df86f60e65ed489c
Diego1234454 <medinadiego607@gmail.com>
2025-09-24 13:58:21 -0500

    Changes to About, ContactSection and Hero sections

--- src/Components/ContactSection.jsx
+++ src/Components/ContactSection.jsx
@@ -1,4 +1,15 @@
-import {ArrowUp, Facebook, Instagram, Linkedin, Mail, MapPin, MessageCircle, Phone, Twitter} from "lucide-react";
+import {
+    ArrowUp,
+    Facebook,
+    Github,
+    Instagram,
+    Linkedin,
+    Mail,
+    MapPin,
+    MessageCircle,
+    Phone,
+    Twitter
+} from "lucide-react";
 
 export const ContactSection = () => {
     return (
@@ -48,7 +59,7 @@ export const ContactSection = () => {
                         <h4 className="font-medium mb-4 pb-2 text-white/80">Connect with me on:</h4>
                         <div className="flex space-x-4 justify-center">
                             <a href="https://www.linkedin.com/in/diego-allan-véliz-medina-29906427a" title="My LinkedIn account" target="_blank"><Linkedin className="text-white/60 hover:text-blue-400/80 transition-colors"/></a>
-                            <a href="https://x.com/Diego36196752" title="My Twitter account" target="_blank"><Twitter className="text-white/60 hover:text-blue-300/80 transition-colors"/></a>
+                            <a href="https://github.com/Diego1234454?tab=repositories" title="My Github account" target="_blank"><Github className="text-white/60 hover:text-white transition-colors"/></a>
                             <a href="https://www.instagram.com/allan_med7/" title="My instagram profile" target="_blank"><Instagram className="text-white/60 hover:text-pink-500/80 transition-colors"/></a>
                             <a href="https://www.facebook.com/allan.veliz.237464/" title="My Facebook profile" target="_blank"><Facebook className="text-white/60 hover:text-blue-500/80 transition-colors"/></a>
                             <a href="https://wa.me/528993175207?" target="_blank" title="My Whatsapp!"><MessageCircle className="text-white/60 hover:text-green-400/80 transition-colors"/></a>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,7 +7,8 @@ export const Hero = () => {
                         <h1 className="text-blue-300 xl:text-4xl lg:text-2xl sm:text-sm  md:text-xl ">Hi, my name is</h1>
                         <h2 className="text-white/80 xl:text-8xl text-2xl sm:text-2xl md:text-4xl lg:text-6xl font-bold">Diego Véliz.</h2>
                         <h3 className="text-white/60 font-semibold text-xl sm:text-xl  md:text-2xl lg:text-4xl xl:text-6xl  ">I'm currently learning web development.</h3>
-                        <p className="text-white/50 text-justify sm:text-sm  md:text-md lg:text-xl xl:text-2xl">Hi! I'm from México and i'm learning web development using <span className="text-blue-300/80">React.JS</span> and <span className="text-blue-400">TailwindCSS</span>, while I keep improving my web development skills I'm pursuing a career in web development. I'm Passionate about what I do and will be posting my projects, skills and everything about my web development career in this website!</p>
+                        <p className="text-white/50 text-justify sm:text-sm  md:text-md lg:text-xl xl:text-2xl">Hi! I'm from México and i'm learning web development using
+                            <span className="text-blue-300/80"><a href="https://react.dev/" target="_blank"> React.JS</a></span> and <span className="text-blue-400"> <a href="https://tailwindcss.com/" target="_blank">TailwindCSS</a></span>, while I keep improving my web development skills I'm pursuing a career in web development. I'm Passionate about what I do and will be posting my projects, skills and everything about my web development career on this website!</p>
                         <a href="#about" className="text-blue-300/80 inset-ring-2 inset-ring-blue-400  px-6 py-3 w-fit font-semibold hover:bg-white/5 ">Learn more about me.</a>
                     </div>
 
